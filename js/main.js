6a7716590245ac9d294a0e6c2a1b99e00871bfb8
AoDWork <aodtest07@gmail.com>
2022-07-11 14:09:17 +0300

    Wooman page + js hide excess text

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
+"use strict"
+window.addEventListener("DOMContentLoaded", ()=>{
+    const textP = document.querySelectorAll('.card p');
+    console.log(textP.innerHTML);
+    
+    textP.forEach( (item, ind) => {
+        if(item.innerHTML.length > 60){
+            item.innerHTML = `${item.innerHTML.substring(0, 60)} ...`;
+            console.log(item.innerHTML);
+        };
+    });
+});
+
+
 // $(window).on('load', function(){
 
 //     //vide.js -videobackground
